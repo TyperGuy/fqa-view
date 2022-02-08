df83c4ce566b6fbbd82d91025dcbb7d51629fa45
TyperGuy <ray.developper@gmail.com>
2022-02-08 02:09:07 +0100

    feat : add State

--- src/components/dropped/Dropped.tsx
+++ src/components/dropped/Dropped.tsx
@@ -1,21 +1,25 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { BsPlusLg } from 'react-icons/bs';
 import { CustomContainer as Flex } from '../../types/Container.type';
 
-const Dropped: FC = () => (
+const Dropped: FC<{
+  question: string;
+  answer: string;
+  toogle: Function;
+  element: string;
+}> = ({ question, answer, toogle, element }) => (
   <Flex style={{ borderTop: 'solid 1px #ddd' }} dir="column">
-    <Flex style={{ color: 'rgb(17, 17, 17)' }} dir="row">
-      <h3>Esse é o Titulo</h3>
+    <Flex
+      style={{ color: 'rgb(17, 17, 17)' }}
+      dir="row"
+      onClick={() => toogle(element)}
+    >
+      <h3>{question}</h3>
       <BsPlusLg />
     </Flex>
     <hr />
     <Flex dir="flex">
-      <p style={{ color: 'rgba(0, 0, 0, 0.6)' }}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.{' '}
-      </p>
+      <p style={{ color: 'rgba(0, 0, 0, 0.6)' }}>{answer}</p>
     </Flex>
   </Flex>
 );
--- src/views/fqa/FQA.tsx
+++ src/views/fqa/FQA.tsx
@@ -1,13 +1,30 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { CustomContainer as Flex } from '../../types/Container.type';
 import Dropped from '../../components/dropped/Dropped';
+import data from '../../api/data.json';
 
-const FQA: FC = () => (
-  <Flex dir="column">
-    <Dropped />
-    <Dropped />
-    <Dropped />
-  </Flex>
-);
+const FQA: FC = () => {
+  const [selected, setSelected] = useState<string>('');
+
+  const toogle = (i: string): string => {
+    if (selected === i) return '';
+    setSelected(i);
+    console.log(i);
+    return '';
+  };
+  return (
+    <Flex dir="column">
+      {data.map((fqa) => (
+        <Dropped
+          question={fqa.question}
+          answer={fqa.answer}
+          toogle={toogle}
+          element={fqa.key}
+          key={fqa.key}
+        />
+      ))}
+    </Flex>
+  );
+};
 
 export default FQA;
